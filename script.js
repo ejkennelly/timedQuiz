deb048069c7d24116daa590ab6d8f3e642fc1340
Eliza Kennelly <elizakennelly@gmail.com>
2020-09-29 22:23:53 -0400

    Created function to call next question and clear old question

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var startButton = document.querySelector("#startButton");
 var highScores = document.querySelector("#scoreContainer");
 var answerButtons = document.querySelector("#buttons");
 var timer = document.querySelector("#timer");
+var answerDisplay = document.querySelector("#answers");
 var timeLeft = 100;
 var quizIndex = 0;
 var quizFinished = false;
@@ -10,38 +11,102 @@ var quizFinished = false;
 var quiz = [
     {
         question: "Commonly used data types DO NOT include:",
-        answers: ["strings", "booleans", "alerts", "numbers"],
+        answer1: "strings",
+        answer2:"booleans",
+        answer3:"alerts",
+        answer4:"numbers",
         correct: "alerts"
     },
     {
         question: "The condition in an if/else statement is enclosed within _______.",
-        answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer1: "quotes",
+        answer2: "curly brackets", 
+        answer3: "parentheses", 
+        answer4: "square brackets",
         correct: "parentheses"
     },
     {
         question: "Arrays in JavaScript can be used to store ______.",
-        answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer1: "numbers and strings",
+        answer2: "other arrays", 
+        answer3: "booleans", 
+        answer4: "all of the above",
         correct: "all of the above"
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer1: "JavaScript", 
+        answer2: "terminal/bash", 
+        answer3: "for loops", 
+        answer4: "console.log",
         correct: "console.log"
     },
     {
         question: "String values must be enclosed within _______ when being assigned to variables.",
-        answers: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer1: "commas",
+        answer2: "curly brackets", 
+        answer3: "quotes",
+        answer4: "parentheses",
         correct: "quotes"
     }
 ]
 // FUNCTIONS
+function displayQuestion(index) {
+    if (index >= quiz.length) {
+        quizFinished = true;
+    }
+    else {
+        mainDisplay.textContent = quiz[index].question;
 
+        for (var i = 0; i < 4; i++) {
+            var quizAnswers = document.createElement("button");
+            quizAnswers.setAttribute("id", (i + 1).toString())
+            if (i == 0)
+                quizAnswers.textContent = quiz[index].answer1;
+            else if (i == 1)
+                quizAnswers.textContent = quiz[index].answer2;
+            else if (i == 2)
+                quizAnswers.textContent = quiz[index].answer3;
+            else
+                quizAnswers.textContent = quiz[index].answer4;
+            answerButtons.append(quizAnswers);
+        }
+    }
+};
+function answerCheck(quizIndex) {
+    var allAnswers = document.querySelectorAll("button");
+    for (var i = 0; i < allAnswers.length; i++) {
+        allAnswers[i].addEventListener("click", function () {
+            if (this.id === quiz[quizIndex].correct) {
+                redraw(quizIndex);
+                answerDisplay.textContent = "Correct!";
+            }
+            else {
+                timeLeft -= 10;
+                redraw(quizIndex);
+                answerDisplay.textContent = "Wrong!"
+            }
+        })
+    }
+};
+function clearButtons() {
+    $("#buttons").on("click", function() {
+        answerButtons.empty(answerDisplay);
+    })
+}
+function redraw(quizIndex){
+    quizIndex++;
+    buttons.innerHTML=""
+    displayQuestion(quizIndex);
+    answerCheck(quizIndex);
+};
 $(document).ready(function () {
 
     //Hide start button on click
     $("#startButton").on("click", function () {
         $(startButton).hide();
-       
+        displayQuestion(quizIndex);
+        answerCheck(quizIndex);
         // Set up timer to start when Start button is clicked
         var quizTimer = setInterval(function () {
             if (timeLeft === -1) {
@@ -55,37 +120,13 @@ $(document).ready(function () {
             timeLeft--;
 
         }, 1000);
-        //function to display quiz questions
-        displayQuestion(quizIndex);
-        // answerCheck(quizIndex);
-        function displayQuestion(index) {
-            if (index >= quiz.length) {
-                quizFinished = true;
-            }
-            else {
-                mainDisplay.textContent = quiz[index].question;
-                
-                for (var i=0; i<4; i++) {
-                    var quizAnswers = document.createElement("button");
-                    quizAnswers.setAttribute("class", "btn btn-primary m-1");
-                    quizAnswers.setAttribute("id", (i + 1).toString())
-                    if (i == 0)
-                        quizAnswers.textContent = quiz[index].answers[i];
-                        else if (i ==1)
-                        quizAnswers.textContent = quiz[index].answers[i];
-                        else if (i == 2)
-                        quizAnswers.textContent = quiz[index].answers[i];
-                        else 
-                        quizAnswers.textContent = quiz[index].answers[i];
-                        answerButtons.append(quizAnswers)
-                }
-            }
-        }
-            //loop through quiz questions
-            //Answer click moves onto next question 
-            //After last question, score screen displays with input for initials
-            //Store time and input in local storage
-        
+
+
+        //loop through quiz questions
+        //Answer click moves onto next question 
+        //After last question, score screen displays with input for initials
+        //Store time and input in local storage
+
 
 
 
